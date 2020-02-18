65f5a031071752eddfbb0d5fb625b27635639071
Robert Driskell <driskell.ba@gmail.com>
2020-02-17 22:07:52 -0600

    messing with backgrounds

---
+++ src/components/name/name.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './name.css'
+
+const Name = () => {
+    return (
+        <div className="name">
+            Robert Driskell
+        </div>
+    )
+}
+
+export default Name
