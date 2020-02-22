90aa2a685240180ab5de2e1ae904333411486120
Robert Driskell <driskell.ba@gmail.com>
2020-02-21 18:02:05 -0600

    Mapping data out of my markdown files

--- src/components/data-test/DataTest.js
+++ src/components/data-test/DataTest.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 
-export const DataTest = () => {
-
-    
+const DataTest = ({data}) => {
 
     return (
         <div>
-            
+            {data.allMarkdownRemark.edges.map(post => (
+                <div>
+                    <h3>{post.node.frontmatter.title}</h3>
+                    {console.log(post)}
+                    {post.node.frontmatter.images !== null ? (
+                        post.node.frontmatter.images.map(img => (
+                            <img src={img.image} />
+                        ))
+                    ) : (
+                        <div>yoyoyo</div>
+                    )}
+                </div>
+            ))}
         </div>
     )
 }
+
+export default () => (
+    <StaticQuery
+        query={graphql`
+            query TestIndexQuery {
+                allMarkdownRemark {
+                    edges {
+                        node {
+                            frontmatter {
+                                title
+                                description
+                                images {
+                                    image
+                                }
+                            } 
+                        }
+                    }
+                }
+            }
+        `}
+        render={( data ) => <DataTest data={data} />}
+    />
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Name from "../components/name/name"
-import { DataTest } from "../components/data-test/DataTest";
+import DataTest from "../components/data-test/DataTest";
 
 const IndexPage = () => (
     <div>
