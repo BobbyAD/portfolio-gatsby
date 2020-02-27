0534ec68c041b40c45bc041a6debe3c6f306c9ea
Robert Driskell <driskell.ba@gmail.com>
2020-02-27 14:36:09 -0600

    Using Netlify's example now

--- src/components/forms/Contact.js
+++ src/components/forms/Contact.js
@@ -1,19 +1,80 @@
-import React from "react"
+import React, { useState } from "react"
 
+
+// Netlify Gatsby basic Contact form
 export const Contact = () => {
+    const [state, setState] = useState({})
+
+    const handleChange = e => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const form = e.target
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({
+                "form-name": form.getAttribute("name"),
+                ...state,
+            }),
+        })
+            .then(() => navigate(form.getAttribute("action")))
+            .catch(error => alert(error))
+    }
+
     return (
         <div>
-            <form name="contact" method="POST" data-netlify="true">
-                <label>
-                    Your Name: <input type="text" name="name" />
-                </label>
-                <label>
-                    Your Email: <input type="email" name="email" />
-                </label>
-                <label>
-                    Message: <textarea name="message"></textarea>
-                </label>
-                <button type="submit">Send</button>
+            <h1>Contact</h1>
+            <form
+                name="contact"
+                method="post"
+                action="/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={handleSubmit}
+            >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <p hidden>
+                    <label>
+                        Don’t fill this out:{" "}
+                        <input name="bot-field" onChange={handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Your name:
+                        <br />
+                        <input
+                            type="text"
+                            name="name"
+                            onChange={handleChange}
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Your email:
+                        <br />
+                        <input
+                            type="email"
+                            name="email"
+                            onChange={handleChange}
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message:
+                        <br />
+                        <textarea name="message" onChange={handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
             </form>
         </div>
     )
