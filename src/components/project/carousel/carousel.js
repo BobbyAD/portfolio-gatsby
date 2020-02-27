0e87a2e462e4d106f923ffa4438bd91be92d1423
Robert Driskell <driskell.ba@gmail.com>
2020-02-26 22:21:44 -0600

    notes for carousel

---
+++ src/components/project/carousel/carousel.js
@@ -0,0 +1,8 @@
+// display current and next, hide next
+// animate images moving left <--
+// current = next
+// instantly swap to current being displayed, no animation
+// next = img[next+1] || img[0];
+// repeat
+// pause if hovered?
+// add arrows?
