0b86a43fbdc61c86e777db991d452ee49246dc58
Robert Driskell <driskell.ba@gmail.com>
2020-02-28 13:11:47 -0600

    removed graphql query in skills for testing

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -7,33 +7,39 @@ const Skills = ({ data }) => {
     return (
         <div>
             <h1>Skills</h1>
-            {/* {data.map(skill => (
+            {data.map(skill => (
                 <Skill skill={skill.skill} key={skill.skill} />
-            ))} */}
+            ))}
         </div>
     )
 }
 
+// export default () => (
+//     <StaticQuery
+//         query={graphql`
+//             query SkillsQuery {
+//                 allMarkdownRemark(
+//                     sort: { order: ASC, fields: [frontmatter___skills___skill] }
+//                     filter: { frontmatter: { templateKey: { eq: "ancillary" } } }
+//                 ) {
+//                     edges {
+//                         node {
+//                             frontmatter {
+//                                 skills {
+//                                     skill
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         `}
+//         render={data => <Skills data={data.allMarkdownRemark.edges[0].node.frontmatter.skills} />}
+//     />
+// )
+
 export default () => (
-    <StaticQuery
-        query={graphql`
-            query SkillsQuery {
-                allMarkdownRemark(
-                    sort: { order: ASC, fields: [frontmatter___skills___skill] }
-                    filter: { frontmatter: { templateKey: { eq: "ancillary" } } }
-                ) {
-                    edges {
-                        node {
-                            frontmatter {
-                                skills {
-                                    skill
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        `}
-        render={data => <Skills data={data.allMarkdownRemark.edges[0].node.frontmatter.skills} />}
-    />
+    <div>
+        asdf
+    </div>
 )
