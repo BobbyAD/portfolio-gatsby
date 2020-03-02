3bd5e5e77d34926735e1abce598797a99b9b424b
Robert Driskell <driskell.ba@gmail.com>
2020-03-02 12:06:04 -0600

    Updated metadata and font

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Robert Driskell Portfolio`,
+        description: `Robert Driskell's Portfolio site. Built using Gatsby and React.`,
+        author: `BobbyAD`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
